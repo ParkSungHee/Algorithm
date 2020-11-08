421dfc9dee5fabeeb89c47450568a5ccbface828
JJJoonngg <whdtls3878@gmail.com>
2020-11-08 18:03:56 +0900

    #13 메인화면 작업
    - 틀만 작업되어있음
    - 임시 코드로 작업해둔상태
    - 데이터와 지도 api 를 받아서 리스너 형태로 넘기는 작업필요

--- app/src/main/java/kr/co/gracegirls/tmi/view/home/HomeFragment.java
+++ app/src/main/java/kr/co/gracegirls/tmi/view/home/HomeFragment.java
@@ -1,30 +1,52 @@
 package kr.co.gracegirls.tmi.view.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.co.gracegirls.tmi.R;
+import kr.co.gracegirls.tmi.data.MountainListItem;
 import kr.co.gracegirls.tmi.module.TitleBar;
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private HomeMountainAdapter homeMountainAdapter;
+    private List<MountainListItem> mountainListItems;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
         TitleBar titleBar = view.findViewById(R.id.home_title_bar);
 
         titleBar.init(getString(R.string.appName), false);
+        recyclerView = view.findViewById(R.id.mountain_list);
 
+        mountainListItems = new ArrayList<>();
+        mountainListItems.add(new MountainListItem("산0", 0, "우리집이다"));
+        mountainListItems.add(new MountainListItem("산1", 10, "우리집이다1"));
+        mountainListItems.add(new MountainListItem("산2", 20, "우리집이다2"));
+        mountainListItems.add(new MountainListItem("산3", 30, "우리집이다3"));
+        mountainListItems.add(new MountainListItem("산4", 40, "우리집이다4"));
+        mountainListItems.add(new MountainListItem("산5", 50, "우리집이다5"));
+
+        homeMountainAdapter = new HomeMountainAdapter(getContext(), mountainListItems);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setAdapter(homeMountainAdapter);
+
+        return view;
     }
 }
---
+++ app/src/main/java/kr/co/gracegirls/tmi/view/home/HomeMountainAdapter.java
@@ -0,0 +1,58 @@
+package kr.co.gracegirls.tmi.view.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import kr.co.gracegirls.tmi.R;
+import kr.co.gracegirls.tmi.data.MountainListItem;
+
+public class HomeMountainAdapter extends RecyclerView.Adapter<HomeMountainAdapter.HomeMountainViewHolder> {
+
+    private List<MountainListItem> mountainList;
+    private Context context;
+    private MountainListItem data;
+
+    public HomeMountainAdapter(Context context, List<MountainListItem> list) {
+        this.context = context;
+        this.mountainList = list;
+    }
+
+    @NonNull
+    @Override
+    public HomeMountainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new HomeMountainViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeMountainViewHolder holder, int position) {
+        data = mountainList.get(position);
+        holder.title.setText(data.getTitle());
+        holder.location.setText(data.getLocation());
+        holder.riskPoint.setText(data.getRiskPoint() + "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountainList.size();
+    }
+
+    class HomeMountainViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView title, riskPoint, location;
+
+        public HomeMountainViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.title = itemView.findViewById(R.id.mountain_title);
+            this.riskPoint = itemView.findViewById(R.id.mountain_risk_point);
+            this.location = itemView.findViewById(R.id.mountain_location);
+        }
+    }
+}
