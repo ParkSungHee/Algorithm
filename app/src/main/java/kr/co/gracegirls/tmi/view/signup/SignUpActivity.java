38e82856fef074d13fc89c5ab5166314d3b8a3e6
JJJoonngg <whdtls3878@gmail.com>
2020-11-16 23:52:12 +0900

    #55 뷰 그리기 완료

---
+++ app/src/main/java/kr/co/gracegirls/tmi/view/signup/SignUpActivity.java
@@ -0,0 +1,16 @@
+package kr.co.gracegirls.tmi.view.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.co.gracegirls.tmi.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
