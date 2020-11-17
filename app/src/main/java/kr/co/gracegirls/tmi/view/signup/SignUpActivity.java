cfdc3e787a8756ab8b7fb44db98164c10c4d2858
JJJoonngg <whdtls3878@gmail.com>
2020-11-17 23:35:56 +0900

    #55 animation 기능 추가
    - 스르륵 스르륵

--- app/src/main/java/kr/co/gracegirls/tmi/view/signup/SignUpActivity.java
+++ app/src/main/java/kr/co/gracegirls/tmi/view/signup/SignUpActivity.java
@@ -3,8 +3,11 @@ package kr.co.gracegirls.tmi.view.signup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import kr.co.gracegirls.tmi.R;
@@ -15,6 +18,9 @@ public class SignUpActivity extends AppCompatActivity {
     ConstraintLayout emailContentBox, nickNameContentBox, passwordContentBox, passwordDuplicateContentBox;
     Button signUpDoneButton;
 
+    private Context context;
+    private Animation translateUp, translateDown;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void init() {
+        context = this;
         TitleBar titleBar = findViewById(R.id.signUpTitleBar);
         titleBar.init("회원가입", false);
 
@@ -33,36 +40,62 @@ public class SignUpActivity extends AppCompatActivity {
         passwordDuplicateContentBox = findViewById(R.id.passwordDuplicateContentBox);
         signUpDoneButton = findViewById(R.id.signUpDoneButton);
 
-        hideNickNameInput();
-        hidePasswordInput();
-        hidePasswordDuplicateInput();
+        translateUp = AnimationUtils.loadAnimation(context, R.anim.translate_up);
+        translateDown = AnimationUtils.loadAnimation(context, R.anim.translate_down);
+
+        OpenMenuAnimationListener openMenuAnimationListener = new OpenMenuAnimationListener();
+        translateUp.setAnimationListener(openMenuAnimationListener);
+        translateDown.setAnimationListener(openMenuAnimationListener);
     }
 
 
     private void showNickNameInput() {
+        nickNameContentBox.startAnimation(translateDown);
         nickNameContentBox.setVisibility(View.VISIBLE);
-
     }
 
     private void hideNickNameInput() {
+        nickNameContentBox.startAnimation(translateUp);
         nickNameContentBox.setVisibility(View.GONE);
     }
 
 
     private void showPasswordInput() {
+        passwordContentBox.startAnimation(translateDown);
         passwordContentBox.setVisibility(View.VISIBLE);
     }
 
     private void hidePasswordInput() {
+        passwordContentBox.startAnimation(translateUp);
         passwordContentBox.setVisibility(View.GONE);
     }
 
     private void showPasswordDuplicateInput() {
+        passwordDuplicateContentBox.startAnimation(translateDown);
         passwordDuplicateContentBox.setVisibility(View.VISIBLE);
     }
 
     private void hidePasswordDuplicateInput() {
+        passwordDuplicateContentBox.startAnimation(translateUp);
         passwordDuplicateContentBox.setVisibility(View.VISIBLE);
+    }
+
+    class OpenMenuAnimationListener implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 
 }
