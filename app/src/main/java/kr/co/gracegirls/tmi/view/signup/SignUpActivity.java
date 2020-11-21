d784b828754e4dc43c7219e4c5538018311821b7
JJJoonngg <whdtls3878@gmail.com>
2020-11-21 17:44:30 +0900

    #57 회원가입 data validation check 완료
    - 정규식 이용하여 이메일 체크
    - 경우에 따라서 닉네임, 비밀번호 보여주기
    - 모든 정보 입력시에만 완료 버튼 클릭 가능하도록

--- app/src/main/java/kr/co/gracegirls/tmi/view/signup/SignUpActivity.java
+++ app/src/main/java/kr/co/gracegirls/tmi/view/signup/SignUpActivity.java
@@ -2,12 +2,15 @@ package kr.co.gracegirls.tmi.view.signup;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Patterns;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +25,10 @@ import kr.co.gracegirls.tmi.module.TitleBar;
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     ConstraintLayout emailContentBox, nickNameContentBox, passwordContentBox, passwordDuplicateContentBox;
-    Button emailDuplicationCheckButton, emailCheckDoneButton, signUpDoneButton;
+    Button emailDuplicationCheckButton, emailCheckDoneButton, signUpDoneButton, signUpDoneButton2, nickNameUseButton, nickNameUseButton2;
     EditText emailInput, nicknameInput, passwordInput, passwordInput2;
+
+    TextView passwordDuplicateTrue, passwordDuplicateFalse;
 
     private Context context;
     private Animation translateUp, translateDown;
@@ -32,6 +37,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private String email, nickName, password, passwordDuplicate;
     FireStoreAccessor fireStoreAccessor = new FireStoreAccessor();
     private SignUpListener signUpListener;
+
+    private boolean nicknameOpen, passwordOpen, passwordOpen2;
 
     @Override
 
@@ -55,13 +62,24 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         emailDuplicationCheckButton = findViewById(R.id.emailDuplicationCheckButton);
         emailDuplicationCheckButton.setOnClickListener(this);
         emailCheckDoneButton = findViewById(R.id.emailDuplicationCheckButton2);
+        emailCheckDoneButton.setOnClickListener(this);
+        emailCheckDoneButton.setClickable(false);
         signUpDoneButton = findViewById(R.id.signUpDoneButton);
         signUpDoneButton.setOnClickListener(this);
+        signUpDoneButton2 = findViewById(R.id.signUpDoneButton2);
+        nickNameUseButton = findViewById(R.id.nicknameUseButton);
+        nickNameUseButton.setOnClickListener(this);
+        nickNameUseButton2 = findViewById(R.id.nicknameUseButton2);
+        nickNameUseButton2.setOnClickListener(this);
+        nickNameUseButton2.setClickable(false);
 
         emailInput = findViewById(R.id.emailInput);
         nicknameInput = findViewById(R.id.nicknameInput);
         passwordInput = findViewById(R.id.passwordInput);
         passwordInput2 = findViewById(R.id.passwordDuplicateInput);
+        passwordDuplicateTrue = findViewById(R.id.passwordDuplicateTrue);
+        passwordDuplicateFalse = findViewById(R.id.passwordDuplicateFalse);
+        textChanging();
 
         translateUp = AnimationUtils.loadAnimation(context, R.anim.translate_up);
         translateDown = AnimationUtils.loadAnimation(context, R.anim.translate_down);
@@ -69,38 +87,102 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         OpenMenuAnimationListener openMenuAnimationListener = new OpenMenuAnimationListener();
         translateUp.setAnimationListener(openMenuAnimationListener);
         translateDown.setAnimationListener(openMenuAnimationListener);
+    }
 
+    private void textChanging() {
+        passwordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() == 4) {
+                    showPasswordDuplicateInput();
+                } else {
+                    hidePasswordDuplicateInput();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        passwordInput2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                password = passwordInput.getText().toString();
+                passwordDuplicate = passwordInput2.getText().toString();
+                if (password.equals(passwordDuplicate)) {
+                    passwordDuplicateTrue.setVisibility(View.VISIBLE);
+                    passwordDuplicateFalse.setVisibility(View.GONE);
+                    signUpDoneButton.setVisibility(View.VISIBLE);
+                    signUpDoneButton2.setVisibility(View.GONE);
+                } else {
+                    passwordDuplicateTrue.setVisibility(View.GONE);
+                    passwordDuplicateFalse.setVisibility(View.VISIBLE);
+                    signUpDoneButton.setVisibility(View.GONE);
+                    signUpDoneButton2.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     private void showNickNameInput() {
+        nicknameOpen = true;
         nickNameContentBox.startAnimation(translateDown);
         nickNameContentBox.setVisibility(View.VISIBLE);
     }
 
     private void hideNickNameInput() {
-        nickNameContentBox.startAnimation(translateUp);
-        nickNameContentBox.setVisibility(View.GONE);
+        if (nicknameOpen) {
+            nicknameInput.setText("");
+            nickNameContentBox.startAnimation(translateUp);
+            nickNameContentBox.setVisibility(View.GONE);
+            nicknameOpen = false;
+        }
     }
 
 
     private void showPasswordInput() {
+        passwordOpen = true;
         passwordContentBox.startAnimation(translateDown);
         passwordContentBox.setVisibility(View.VISIBLE);
     }
 
     private void hidePasswordInput() {
-        passwordContentBox.startAnimation(translateUp);
-        passwordContentBox.setVisibility(View.GONE);
+        if (passwordOpen) {
+            passwordInput.setText("");
+            passwordContentBox.startAnimation(translateUp);
+            passwordContentBox.setVisibility(View.GONE);
+            passwordOpen = false;
+        }
     }
 
     private void showPasswordDuplicateInput() {
+        passwordOpen2 = true;
         passwordDuplicateContentBox.startAnimation(translateDown);
         passwordDuplicateContentBox.setVisibility(View.VISIBLE);
     }
 
     private void hidePasswordDuplicateInput() {
-        passwordDuplicateContentBox.startAnimation(translateUp);
-        passwordDuplicateContentBox.setVisibility(View.VISIBLE);
+        if (passwordOpen2) {
+            passwordInput2.setText("");
+            passwordDuplicateTrue.setVisibility(View.GONE);
+            passwordDuplicateFalse.setVisibility(View.GONE);
+            passwordDuplicateContentBox.startAnimation(translateUp);
+            passwordDuplicateContentBox.setVisibility(View.GONE);
+            passwordOpen2 = false;
+        }
     }
 
     private void listenerInit() {
@@ -128,6 +210,15 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                     Toast.makeText(context, getString(R.string.email_validation_check_false), Toast.LENGTH_SHORT).show();
                 }
                 break;
+            case R.id.emailDuplicationCheckButton2:
+                emailResetting();
+                break;
+            case R.id.nicknameUseButton:
+                nickNameUsingThis(true);
+                break;
+            case R.id.nicknameUseButton2:
+                nickNameUsingThis(false);
+                break;
             case R.id.signUpDoneButton:
 
                 break;
@@ -135,21 +226,66 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    private void nickNameUsingThis(boolean flag) {
+        ConstraintLayout constraintLayout = findViewById(R.id.nicknameContentBox);
+        ConstraintSet constraintSet = new ConstraintSet();
+        if (flag) {
+            nickName = nicknameInput.getText().toString();
+            nickNameUseButton.setVisibility(View.GONE);
+            nicknameInput.setFocusable(false);
+            nickNameUseButton2.setVisibility(View.VISIBLE);
+            nickNameUseButton2.setClickable(true);
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.nicknameInput, ConstraintSet.END, R.id.nicknameUseButton2, ConstraintSet.START);
+            constraintSet.applyTo(constraintLayout);
+            showPasswordInput();
+        } else {
+            nickNameUseButton.setClickable(true);
+            nickNameUseButton.setVisibility(View.VISIBLE);
+            nicknameInput.setFocusable(true);
+            nickNameUseButton2.setVisibility(View.GONE);
+            nickNameUseButton2.setClickable(false);
+            constraintSet.clone(constraintLayout);
+            constraintSet.connect(R.id.nicknameInput, ConstraintSet.END, R.id.nicknameUseButton, ConstraintSet.START);
+            constraintSet.applyTo(constraintLayout);
+            hidePasswordInput();
+            hidePasswordDuplicateInput();
+        }
+    }
+
     private void emailDuplicateCheck(boolean isExist) {
         if (!isExist) {
             emailDuplicationCheckButton.setClickable(false);
             emailInput.setClickable(false);
             emailCheckDoneButton.setVisibility(View.VISIBLE);
+            emailCheckDoneButton.setClickable(true);
             emailDuplicationCheckButton.setVisibility(View.GONE);
             ConstraintLayout constraintLayout = findViewById(R.id.emailContentBox);
             ConstraintSet constraintSet = new ConstraintSet();
             constraintSet.clone(constraintLayout);
             constraintSet.connect(R.id.emailInput, ConstraintSet.END, R.id.emailDuplicationCheckButton2, ConstraintSet.START);
             constraintSet.applyTo(constraintLayout);
+            Toast.makeText(this, "사용가능한 이메일입니다.", Toast.LENGTH_SHORT).show();
             showNickNameInput();
         } else {
             Toast.makeText(this, "존재하는 이메일입니다.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void emailResetting() {
+        emailInput.setText("");
+        emailInput.setClickable(true);
+        emailCheckDoneButton.setVisibility(View.GONE);
+        emailDuplicationCheckButton.setVisibility(View.VISIBLE);
+        emailDuplicationCheckButton.setClickable(true);
+        ConstraintLayout constraintLayout = findViewById(R.id.emailContentBox);
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.connect(R.id.emailInput, ConstraintSet.END, R.id.emailDuplicationCheckButton, ConstraintSet.START);
+        constraintSet.applyTo(constraintLayout);
+        hideNickNameInput();
+        hidePasswordInput();
+        hidePasswordDuplicateInput();
     }
 
 
