21a3d4c0e05e17dcce2b9f2b9df383d3d3049213
ParkSungHee <di6223@naver.com>
2022-07-22 02:26:40 +0900

    [BOJ] 1026 보물

--- baekjoon/project/1026_보물.cpp
+++ baekjoon/project/1026_보물.cpp
@@ -1,44 +1,30 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int dist[1001][1001][1001];
-int map[1001][1001];
-bool visit[1001];
-int n, m, v;
-
-void BFS(int v) {
-    queue<pair<int, pair<int, int>>> q; //cnt, x, y
-    dist[0][1][1] = 1;
-
-    visit[v] = true;
-
-    while (!q.empty()) {
-        int i = q.front().
-
-        for (int i = 1; i <= n; i++) {
-            if (map[v][i] == 0 || visit[i])
-                continue;
-            q.push(i);
-            visit[i] = true;
-        }
-    }
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int a, b;
-    cin >> n >> m >> v;
+    int n, num, sum = 0;
+    vector<int> a, b;
+    cin >> n;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> map[b][a];
-        }
+        cin >> num;
+        a.push_back(num);
     }
-    BFS(v);
+
+    for(int j=0;j<n;j++){
+        cin >> num;
+        b.push_back(num);
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end(), greater<>());
+
+    for(int i=0;i<n;i++){
+        sum += a[i] * b[i];
+    }
+
+    cout << sum;
     return 0;
 }
