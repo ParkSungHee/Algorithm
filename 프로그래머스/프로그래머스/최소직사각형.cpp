e9959f16e08442c89b7b868f52345eb8db9535b3
ParkSungHee <di6223@naver.com>
2022-09-16 19:23:53 +0900

    [PGS] 최소직사각형

---
+++ 프로그래머스/프로그래머스/최소직사각형.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer;
+    vector<int> max_v, min_v;
+
+    for (auto &i: sizes) {
+        if (i[0] < i[1]) { //작은게 min 큰게 max 들어가도록
+            min_v.push_back(i[0]);
+            max_v.push_back(i[1]);
+        } else {
+            min_v.push_back(i[1]);
+            max_v.push_back(i[0]);
+        }
+    }
+
+    sort(min_v.begin(), min_v.end(), greater<>());
+    sort(max_v.begin(), max_v.end(), greater<>());
+
+    answer = min_v[0] * max_v[0];
+    return answer;
+}
+
+int main() {
+    vector<vector<int>> sizes = {{60, 50},
+                                 {30, 70},
+                                 {60, 30},
+                                 {80, 40}};
+    cout << solution(sizes);
+
+    return 0;
+}
